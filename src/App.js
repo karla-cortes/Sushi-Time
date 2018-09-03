39983fa99e3ca9650075fdfc6a60b4c80f65b537
Karla Cortes <kmcortes7@gmail.com>
2018-09-02 23:44:54 -0400

    Added React-Google-Maps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import Input from './components/Input';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Test 123</h1>
-      </div>
+      <Map google={this.props.google} zoom={14}>
+
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+
+        <InfoWindow onClose={this.onInfoWindowClose}>
+        </InfoWindow>
+      </Map>
     );
   }
 }
 
-export default App;
+
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyBGSDclHghewEGK4EYVJooH8vFxWtM93bY')
+})(App)
